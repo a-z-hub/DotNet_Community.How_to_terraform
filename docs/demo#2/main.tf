1c8a1859123d38a1bf10c08a9c6845a86f79b844
Zimm <antoxa.zimm@gmail.com>
2021-07-09 23:22:27 +0200

    limit merge types

--- docs/demo#2/main.tf
+++ docs/demo#2/main.tf
@@ -50,7 +50,33 @@ resource "azuredevops_branch_policy_min_reviewers" "min-2-reviewers" {
   }
 }
 
-resource "azuredevops_project_features" "my-project-features" {
+resource "azuredevops_branch_policy_merge_types" "limit-merge-types" {
+  project_id = data.azuredevops_project.imported-default.id
+
+  enabled  = true
+  blocking = true
+
+  settings {
+    allow_squash                  = false
+    allow_rebase_and_fast_forward = true
+    allow_basic_no_fast_forward   = true
+    allow_rebase_with_merge       = false
+
+    scope {
+      repository_id  = data.azuredevops_git_repository.imported-repository-1.id
+      repository_ref = data.azuredevops_git_repository.imported-repository-1.default_branch
+      match_type     = "Exact"
+    }
+
+    scope {
+      repository_id  = data.azuredevops_git_repository.imported-repository-2.id
+      repository_ref = data.azuredevops_git_repository.imported-repository-2.default_branch
+      match_type     = "Exact"
+    }
+  }
+}
+
+resource "azuredevops_project_features" "project-features" {
   project_id = data.azuredevops_project.imported-default.id
   features = {
     "boards"    = "enabled"
